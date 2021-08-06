9e3e606b445fb9e7290bffa27f8357247387f3e6
profjaquetafine <88409934+profjaquetafine@users.noreply.github.com>
2021-08-06 11:22:25 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -96,7 +96,7 @@ function draw() {
     
     if((touches.length > 0 || keyDown("SPACE")) && trex.y  >= height-120) {
       somDePulo.play( )
-      trex.velocityY = -10;
+      trex.velocityY = -15;
       
     }
     
